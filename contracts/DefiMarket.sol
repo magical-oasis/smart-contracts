64c1fbee556f82489138d3ee53219c28afe934ac
Marc-Olivier Bouchard <mo.bouchard1997@gmail.com>
2021-07-20 19:54:36 -0400

    + change listing for item

--- contracts/DefiMarket.sol
+++ contracts/DefiMarket.sol
@@ -6,7 +6,7 @@ contract DefiMarket {
   
     address payable public minter;
     mapping (address => uint) public balances;
-    mapping (address => string) public items;
+    mapping (address => uint8) public listingIds;
     mapping (address => address payable) public tradeReceivers;
 
 
@@ -26,30 +26,18 @@ contract DefiMarket {
         
         balances[buyer] = 0;
         tradeReceivers[buyer] = address(0);
-        items[buyer] = "";
+        listingIds[buyer] = 0;
     }
     
-    function addTradeOffer(address payable receiver, string memory itemId)  public payable
+    function addTradeOffer(address payable receiver, uint8 listingId)  public payable
     {
         require(msg.value != 0, "You need to send ETH to buy a skin!");
 
         payable(address(this)).transfer(msg.value);
         
         balances[msg.sender] = msg.value;
-        items[msg.sender] = itemId;
+        listingIds[msg.sender] = listingId;
         tradeReceivers[msg.sender] = receiver;
-    }
-    
-    function balanceOf(address _tokenHolder) public view returns (uint) {
-        return balances[_tokenHolder];
-    }
-    
-    function getMyBalance() public view returns (uint) {
-        return balances[msg.sender];
-    }
-    
-    function getMyTrade() public view returns (string memory, address, uint) {
-        return (items[msg.sender], tradeReceivers[msg.sender], balances[msg.sender]);
     }
 
     function withdraw(uint amount) public payable {
