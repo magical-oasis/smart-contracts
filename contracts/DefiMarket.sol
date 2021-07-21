bebad99f5c708cf5b7d2e041d416626182ea07a3
Michael Poirier-Ginter <michael.poirierginter@gmail.com>
2021-07-21 19:51:02 -0400

    added property

--- contracts/DefiMarket.sol
+++ contracts/DefiMarket.sol
@@ -1,60 +1,59 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.0;
 
-
 contract DefiMarket {
-  
     address payable public minter;
-    mapping (address => uint) public balances;
-    mapping (address => uint) public listingIds;
-    mapping (address => address payable) public tradeReceivers;
-
+    mapping(address => uint256) public balances;
+    mapping(address => uint256) public listingIds;
+    mapping(address => address payable) public tradeReceivers;
+    mapping(uint256 => address[]) public listingIdsWithBuyingOffers;
 
     constructor() public payable {
         minter = msg.sender;
     }
-    
 
-    function wakeup(address payable buyer)  public payable
-    {
+    function wakeup(address payable buyer) public payable {
         require(msg.sender == minter, "Only the owner can call this.");
-        
-        uint amountToSend = balances[buyer];
+
+        uint256 amountToSend = balances[buyer];
         address payable receiver = tradeReceivers[buyer];
-        
+
         receiver.transfer(amountToSend);
-        
+
         balances[buyer] = 0;
         tradeReceivers[buyer] = address(0);
         listingIds[buyer] = 0;
     }
-    
-    function addTradeOffer(address payable receiver, uint listingId)  public payable
+
+    function addTradeOffer(address payable receiver, uint256 listingId)
+        public
+        payable
     {
         require(msg.value != 0, "You need to send ETH to buy a skin!");
 
         payable(address(this)).transfer(msg.value);
-        
+
         balances[msg.sender] = msg.value;
         listingIds[msg.sender] = listingId;
         tradeReceivers[msg.sender] = receiver;
+        listingIdsWithBuyingOffers[listingId].push(msg.sender);
     }
 
-    function withdraw(uint amount) public payable {
+    function withdraw(uint256 amount) public payable {
         require(msg.sender == minter, "Only the owner can call this.");
 
         msg.sender.transfer(amount);
     }
-    
-    function getContractBalance() public view returns(uint){
+
+    function getContractBalance() public view returns (uint256) {
         return address(this).balance;
     }
-    
+
     function deposit() public payable {
         require(msg.value != 0, "Useless to deposit 0 ETH");
 
         payable(address(this)).transfer(msg.value);
     }
-    
+
     receive() external payable {}
 }
